e75d5ce47969e8059e914c995f2b72c4160c9faf
BryanNilsen <bryannilsen@gmail.com>
2020-02-12 11:14:02 -0600

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+console.log("WuzzApp Connected!")
+
+// TODO: Get all Entries and Render to DOM
+// steps??
+
+// TODO: Add (Create) new entry and add to Database
+// steps ??
+
+// TODO: Delete entry from database
+// steps ??
